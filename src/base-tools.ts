cc9a965bf2bdc5e7f0016af216b87f367c932148
Éverton M. Vieira <emuvi@outlook.com.br>
2023-02-28 09:34:52 -0300

    initial development of the base export

--- src/base-tools.ts
+++ src/base-tools.ts
@@ -1,14 +1,46 @@
-import { QinColumn, QinExpect, QinLabel, QinModule } from "qin_case";
+import {
+  QinButton,
+  QinColumn,
+  QinExpect,
+  QinHeader,
+  QinLabel,
+  QinModule,
+  QinText,
+} from "qin_case";
+import { QinEvent } from "qin_soul";
 
 export class BaseTools extends QinColumn {
   private _module: QinModule;
   private _expect?: QinExpect;
-  
+
+  private _header = new QinHeader(1, "Export CSV");
+  private _button = new QinButton({ label: new QinLabel("Export") });
+  private _output = new QinText({ rows: 10 });
+
   constructor(module: QinModule, expect?: QinExpect) {
     super();
     this._module = module;
     this._expect = expect;
-    let label = new QinLabel("Base Tools, testing...");
-    label.install(this);
+    this._header.install(this);
+    this._button.install(this);
+    this._button.addActionMain(this._buttonAction);
+    this._output.install(this);
   }
+
+  private _buttonAction = (ev: QinEvent) => {
+    this.qinpel.talk.giz
+      .run({
+        exec: "argo/base-export.giz",
+      })
+      .then((token) => {
+        this.qinpel.talk.issued
+          .askWhenDone({
+            token: token,
+            askOutLines: true,
+          })
+          .then((result) => (this._output.value = result.outLines))
+          .catch((err) => this.qinpel.jobbed.showError(err, "{qia_argo}(ErrCode-000002)"));
+      })
+      .catch((err) => this.qinpel.jobbed.showError(err, "{qia_argo}(ErrCode-000001)"));
+  };
 }
